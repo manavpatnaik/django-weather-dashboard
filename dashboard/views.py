ddc4dbced315e563511245c2fc64f5840abedc94
Manav Patnaik <manavpatnaik@hotmail.com>
2020-12-31 19:21:42 +0530

    Designed the Templates

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 def home(request):
-    return HttpResponse('This is the Home Page')
+    weather = requests.get('http://api.openweathermap.org/data/2.5/weather?q=coimbatore&appid=6d37ed00a79179857e7f18eaaeef43fe')
+    print(weather)
+    return render(request, 'home.html')
